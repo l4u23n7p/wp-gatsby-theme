00a5db819e4df01b82acfb226081155a6abf9cae
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-01-12 15:34:21 +0100

    Add custom post type of project

--- functions.php
+++ functions.php
@@ -13,6 +13,30 @@
  * Based on example at: https://codex.wordpress.org/Function_Reference/register_post_type
  */
 
+add_action('init', 'custom_post_types');
+function custom_post_types()
+{
+    register_post_type(
+        'project',
+        array(
+            'labels' => array('name' => __('Projects'), 'singular_name' => __('Project')),
+            'public' => true,
+            'has_archive' => true,
+            'public' => true,
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
+            'show_ui' => true,
+            'show_in_menu' => true,
+            'show_in_nav_menus' => true,
+            'show_in_admin_bar' => true,
+            'menu_icon' => 'dashicons-welcome-learn-more',
+        )
+    );
+}
 
-
- // Add theme functions here
+add_action('publish_post', 'post_published_notification', 10, 2);
+function post_published_notification($ID, $post)
+{
+    //wp_remote_post('https://api.netlify.com/build_hooks/xxxxxxxxxxxxxxxxxxxx');
+    //will add later on prod
+}
