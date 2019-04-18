1f35eddad562812e87d0a4bfdf72cd1ecc09cfa8
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-04-18 20:25:23 +0200

    feat: featured media

--- functions.php
+++ functions.php
@@ -299,16 +299,8 @@ function get_post_categories($data, $field, $request)
     }
     return $formatted_categories;
 }
-function get_post_images($data, $field, $request)
+function get_post_featured_media($data, $field, $request)
 {
-    $medium = wp_get_attachment_image_src(get_post_thumbnail_id($data->id), 'medium');
-    $medium_url = $medium['0'];
-
-    $large = wp_get_attachment_image_src(get_post_thumbnail_id($data->id), 'large');
-    $large_url = $large['0'];
-
-    return array(
-        'medium' => $medium_url,
-        'large' => $large_url,
-    );
+    $featured_media_url = wp_get_attachment_image_src(get_post_thumbnail_id($data->id))['0'];
+    return $featured_media_url;
 }
