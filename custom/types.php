d20c85dc82f6345ab6820819ff91beecdcc4533c
Laurent-PANEK <laurent.panek@ynov.com>
2019-04-24 17:15:50 +0200

    feat: taxonomy custom columns

--- custom/types.php
+++ custom/types.php
@@ -5,6 +5,12 @@
  */
 
 add_action('init', 'custom_post_types');
+add_action('init', 'add_project_taxonomy', 20);
+add_filter('manage_edit-category_columns' , 'custom_taxonomy_columns');
+add_filter('manage_edit-filter_columns' , 'custom_taxonomy_columns');
+add_filter( 'manage_category_custom_column', 'category_columns_content', 10, 3 );
+add_filter( 'manage_filter_custom_column', 'filter_columns_content', 10, 3 );
+
 function custom_post_types()
 {
     register_post_type(
@@ -25,7 +31,7 @@ function custom_post_types()
         )
     );
 }
-add_action('init', 'add_project_taxonomy');
+
 function add_project_taxonomy()
 {
     register_taxonomy(
@@ -40,7 +46,7 @@ function add_project_taxonomy()
                 'edit_item' => __('Edit Filter'),
                 'view_item' => __('Show Filter'),
                 'update_item' => __('Update Filter'),
-                'add_new_item' => __('Add Filter',
+                'add_new_item' => __('Add Filter'),
                 'new_item_name' => __('New Filter'),
                 'search_items' => __('Search Filters'),
                 'popular_items' => __('Popular Filters'),
@@ -55,4 +61,36 @@ function add_project_taxonomy()
             'show_in_nav_menus' => true,
         )
     );
+}
+
+function custom_taxonomy_columns( $columns )
+{
+	$columns['color'] = __('Color');
+	$columns['text_color'] = __('Text Color');
+
+	return $columns;
+}
+
+function taxonomy_columns_content( $taxonomy, $content, $column_name, $term_id )
+{
+    $term = get_term( $term_id, $taxonomy);
+
+    if ('color' == $column_name) {
+        $color = get_field('color', $term);
+        $content = '<div style="background-color: ' . $color . ';height: 25px;box-shadow: 1px 1px 4px black;">';
+    }
+    if ( 'text_color' == $column_name ) {
+        $text_color = get_field('text_color', $term);
+        $content = '<div style="background-color: ' . $text_color . ';height: 25px;box-shadow: 1px 1px 4px black;">';
+		
+    }
+	return $content;
+}
+
+function category_columns_content( $content, $column_name, $term_id) {
+    return taxonomy_columns_content( 'category', $content, $column_name, $term_id );
+}
+
+function filter_columns_content( $content, $column_name, $term_id) {
+    return taxonomy_columns_content( 'filter', $content, $column_name, $term_id );
 }
