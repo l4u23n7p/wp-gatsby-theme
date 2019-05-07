509eaa9cc24bf8233500f0499ade1834e5ba4ec8
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-05-07 14:53:38 +0200

    feat: update file version (#14)

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ add_theme_support( 'html5' ,
 
 function load_custom_wp_admin_style()
 {
-    wp_register_style('custom_wp_admin_css', get_template_directory_uri() . '/css/custom-admin-style.css', false, '1.0.0');
+    wp_register_style('custom_wp_admin_css', get_template_directory_uri() . '/css/custom-admin-style.css', false, '20190507145010');
     wp_enqueue_style('custom_wp_admin_css');
 }
 add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style');
